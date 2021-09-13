c5a41d52c6c7d50f5b92e09a3e318f91412a3213
Gustavo Lorenzo <tavolorenzo1@gmail.com>
2021-09-12 21:40:20 -0300

    Update sentencesDB.py

--- app/data/databases/sentencesDB.py
+++ app/data/databases/sentencesDB.py
@@ -1,1 +1,8 @@
 import sqlite3
+create_role_table='''
+CREATE TABLE role (
+    roleId INTEGER PRIMARY KEY AUTOINCREMENT,
+    description varchar(20) NOT NULL,
+    access varchar(50) NOT NULL,
+)
+'''
