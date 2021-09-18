8602711d5bfff97ef66dbd4da3fadcb9ba96b296
Camila Sosa <90394716+sosa99cami@users.noreply.github.com>
2021-09-18 01:01:38 +0000

    services.task

--- app/server.py
+++ app/server.py
@@ -14,9 +14,7 @@ app = Flask(__name__)
 #DEFINIR FUCION 
 
 @app.route('/users/new',methods=['POST'])
-def create_users():
-    user_info = request.get_json()
-    if 'name' not in user_info:
+if 'name' not in user_info:
         return 'El nombre de usuario es requerido', 412
     if 'lastName' not in user_info:
         return 'El nombre de usuario es requerido', 412
@@ -24,9 +22,17 @@ def create_users():
         return 'La clave es requerida', 412    
     user.create_user(user_info['document'], user_info['name'], user_info['lastname'], user_info['phone'], user_info['email'], user_info['photoURI'], user_info['password'], user_info['jobposition'], user_info['roleId'])
     return 'OK', 200
+  
 
 @app.route('/tasks/new',methods=['POST'])
-#DEFINIR FUCION 
+    def create_task():
+    task_model = request.get_json()
+    if 'roleId' not in user_infotask_model:
+        return 'El campo rol es requerido', 412
+    if 'title' not in user_info:
+        return 'El titulo es requerido', 412
+    task.create_task(task_model['roleId'], task_model['title'])
+    return 'OK', 200
 
 @app.route('/tasks/{taskId}',methods=['PUT'])
 #DEFINIR FUCION 
